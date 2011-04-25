356be763429deef40d09b7976b9419c556dda1d6
Rob Terhaar <rob@atlanticdynamic.com>
2011-04-25 11:28:43 -0400

    moved variable names to lowercase to fit puppet module coding standards, and renamed the FQDN variable to dynect_record to reduce confusion about what we're actually managing with this module

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,35 +5,35 @@
 # 4 - Failover (main ip address)
 # 5 - Failover (failover ip address)
 #
-$DYNECT_TYPE = 1
+$dynect_type = 1
 
 #Required for adding records to any dynect account
-$CUSTOMER_NAME = "<customer name>"
-$USER_NAME = "<user name>"
-$PASSWORD = "<password>"
-$ZONE = "<zone>"
-$FQDN = "<fqdn>"
+$customer_name = "<customer name>"
+$user_name = "<user name>"
+$password = "<password>"
+$zone = "<zone>"
+$dynect_record = "<fqdn>"
 
 # This is only required if you are using GSLB
-$REGION = "<region>"
+$region = "<region>"
 
 
 class dyn_module {
-	case $DYNECT_TYPE {
+	case $dynect_type {
 		1: {
-			add_a_record($CUSTOMER_NAME, $USER_NAME, $PASSWORD, $ZONE, $FQDN)
+			add_a_record($customer_name, $user_name, $password, $zone, $dynect_record)
 		}
 		2: {
-			add_address_to_gslb($CUSTOMER_NAME, $USER_NAME, $PASSWORD, $ZONE, $FQDN, $REGION)
+			add_address_to_gslb($customer_name, $user_name, $password, $zone, $dynect_record, $region)
 		}
 		3: {
-			add_address_to_load_balance($CUSTOMER_NAME, $USER_NAME, $PASSWORD, $ZONE, $FQDN)
+			add_address_to_load_balance($customer_name, $user_name, $password, $zone, $dynect_record)
 		}
 		4: {
-			update_failover_address($CUSTOMER_NAME, $USER_NAME, $PASSWORD, $ZONE, $FQDN)
+			update_failover_address($customer_name, $user_name, $password, $zone, $dynect_record)
 		}
 		5: {
-			update_failover_foaddress($CUSTOMER_NAME, $USER_NAME, $PASSWORD, $ZONE, $FQDN)
+			update_failover_foaddress($customer_name, $user_name, $password, $zone, $dynect_record)
 		}
 	}
 }
